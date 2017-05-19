728c8b123099eed44a00b50a1f645af353281575
wuyuelong <915855398@qq.com>
2017-05-19 16:59:45 +0800

    1

--- ssm/src/main/java/com/yhx/service/LoginService.java
+++
@@ -1,9 +0,0 @@
-package com.yhx.service;
-
-import com.yhx.po.Login;
-
-import cn.yhx.exception.UserException;
-
-public interface LoginService {
-	public Login  getLogin(String username) throws UserException;
-}
